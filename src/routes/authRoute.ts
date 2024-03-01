faf38f8e1c81540373ee512a10bca5664895e656
barat-sh <barath.elangovan@icloud.com>
2024-03-01 20:07:01 +0530

    created routed for backend

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -3,9 +3,10 @@ const router = experss.Router();
 
 // importing controllers
 import { Register, Login, getProfile } from "../controller/AuthService";
+import { jwtValidateToken } from "../config/Authentication";
 
 // using routes
 router.post("/register", Register);
 router.post("/login", Login);
-router.get("/getProfile", getProfile);
+router.get("/getProfile", jwtValidateToken, getProfile);
 export default router;
